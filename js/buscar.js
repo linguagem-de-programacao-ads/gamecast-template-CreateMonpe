4d1cd4b0e916d29aa1c22549f38cac584005cee2
CreateMonpe <pedro.gomes@sptech.school>
2024-04-18 21:18:29 -0300

    Adicionando novos cards

--- js/buscar.js
+++ js/buscar.js
@@ -1,5 +1,5 @@
 async function buscar(){
-    console.log("Passei por aqui")
+
 
     const resposta = await fetch("https://66186bb59a41b1b3dfbd0717.mockapi.io/gamecast/agendas");
     const respostaDadosAgenda = await resposta.json();
@@ -9,11 +9,34 @@ async function buscar(){
     const cards = document.getElementById("cards_games")
 
     cards.innerHTML = respostaDadosAgenda.map((itemAgenda) => {
-        return 
-    });
+        return `
+        <div class="cardItem">
+                <div class="dataGame"> 
+                    <img src="../imagens/calendar-solid.svg" alt=""> ${itemAgenda.dataJogo}
+                </div>
+                <img class="img-background" src="${itemAgenda.urlImagem}" alt="">
+                <div class="descricao">
+                    <p> <strong>${itemAgenda.nome}</strong> </p>
+                    <p>${itemAgenda.descricao}</p>
+                    <p><strong>Gamers:  </strong></p>
+
+                    <div class="gamers">
+                    ${
+                        itemAgenda.gamers.map((gamerItem) => {
+                            return `<div class="gamerItem">@${gamerItem}</div>`
+                        }).join('')
+                    }
+                    </div>
+
+                  
+                </div>  
+                <div class="assistir">
+                        <a class="assistirItem"> <img src="../imagens/youtube.svg" alt=""> Assistir </a>
+                </div>
+            </div>
+        `
+    }).join('');
 
 }
 
-console.log("antes de buscar")
 buscar();
-console.log("depois de buscar")
